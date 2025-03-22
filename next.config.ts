1973841aca3ce04a198f24039d08526254510dc2
mehansumit <159796335+mehansumit@users.noreply.github.com>
2025-03-22 06:13:45 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/french-learning-app',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
 
 export default nextConfig;
