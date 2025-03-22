21196d5af825993e674484a8c33d12bb8d241d90
mehansumit <159796335+mehansumit@users.noreply.github.com>
2025-03-22 06:51:54 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const NextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+export default NextConfig;
